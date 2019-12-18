903add76041bbdf048cc4bd4d36a7db4da32eb69
liuyatao <liuyatao@boco.com.cn>
2019-12-18 17:31:00 +0800

    mockMvc

--- src/main/java/com/github/liuyatao/springbasic/UserDTO.java
+++ src/main/java/com/github/liuyatao/springbasic/UserDTO.java
@@ -2,6 +2,7 @@ package com.github.liuyatao.springbasic;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * UserResource
@@ -9,6 +10,7 @@ import lombok.Data;
 
  @Data
  @AllArgsConstructor
+ @NoArgsConstructor
 public class UserDTO {
 
     private String name;
--- src/test/java/com/github/liuyatao/springbasic/UserResourceTest.java
+++ src/test/java/com/github/liuyatao/springbasic/UserResourceTest.java
@@ -1,8 +1,15 @@
 package com.github.liuyatao.springbasic;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -10,16 +17,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.liuyatao.springbasic.service.RegisterUseCase;
 import com.github.liuyatao.springbasic.web.resource.UserResource;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
+import org.mockito.ArgumentCaptor;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 /**
  * UserResourceTest
@@ -27,6 +34,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @WebMvcTest(controllers = { UserResource.class })
 @ExtendWith(MockitoExtension.class)
 public class UserResourceTest {
+
 
     @Autowired
     private MockMvc mockMvc;
@@ -37,17 +45,39 @@ public class UserResourceTest {
     @MockBean
     private RegisterUseCase mockRegisterUseCase;
 
+    @Test
+    public void helloworldTest() throws Exception {
+        mockMvc.perform(get("/helloWorld"))
+        .andExpect(status().isOk())
+        .andExpect(content().string("Hello World"));
+    }
 
     @Test
     public void whenValidInput_thenReturns200() throws JsonProcessingException, Exception {
-        UserDTO userDTO = new UserDTO("Zaphod", "zaphod@galaxy.net");
+        UserDTO userDTO = new UserDTO("刘亚涛", "739697044@qq.com");
 
-        mockMvc.perform(post("/forums/{forumId}/register",42L)
+        when(mockRegisterUseCase.registerUser(any(UserDTO.class), any(Boolean.class)))
+                .thenReturn(new User("UserName","email"));
+
+
+        MvcResult mvcResult = mockMvc.perform(post("/forums/{forumId}/register",42L)
                         .contentType(MediaType.APPLICATION_JSON)
                         .param("sendWelcomeMail", "true")
                         .content(objectMapper.writeValueAsString(userDTO)))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andReturn();
 
-    }
-    
+        //参数捕获
+        ArgumentCaptor<UserDTO> uArgumentCaptor = ArgumentCaptor.forClass(UserDTO.class);
+
+        verify(mockRegisterUseCase, times(1)).registerUser(uArgumentCaptor.capture(), eq(true));
+
+        assertThat(uArgumentCaptor.getValue().getName(),is("刘亚涛"));
+        assertThat(uArgumentCaptor.getValue().getEmail(),is("739697044@qq.com"));
+        
+        //验证返回结果
+        UserDTO actual = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), UserDTO.class);
+        assertThat(actual.getEmail(), is("email"));
+        assertThat(actual.getName(), is("UserName"));
+    }    
 }
